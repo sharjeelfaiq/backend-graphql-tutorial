b483612475ced4423d0ba97686826e708c9f824e
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2025-12-04 18:01:40 +0500

    code updated

--- src/main.js
+++ src/main.js
@@ -2,44 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express4";
+import { userResolvers, userTypeDefs } from "./user/index.js";
 
 dotenv.config();
+
+const PORT = process.env.PORT;
 
 (async function main() {
   const app = express();
   app.use(express.json());
-  const PORT = process.env.PORT;
-
-  // const typeDefs = gql`
-  //   type Query {
-  //     hello: String
-  //     greeting: String
-  //   }
-
-  //   type Mutation {
-  //     hello: String
-  //     greeting: String
-  //   }
-  // `;
-
-  // const resolvers = {
-  //   Query: {
-  //     hello: () => "Hello world",
-  //     greeting: () => "Good day",
-  //   },
-
-  //   Mutation: {
-  //     hello: () => "Hello world",
-  //     greeting: () => "Good day",
-  //   },
-  // };
 
   const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
+    typeDefs: [userTypeDefs],
+    resolvers: [userResolvers],
   });
 
   await apolloServer.start();
+
   app.use("/graphql", expressMiddleware(apolloServer));
 
   app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
---
+++ src/user/index.js
@@ -0,0 +1,43 @@
+import { gql } from "graphql-tag";
+
+export const userTypeDefs = gql`
+  type User {
+    id: ID!
+    email: String!
+  }
+
+  input CreateUserInput {
+    email: String!
+    password: String!
+  }
+
+  type Query {
+    userList: [User!]!
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User
+  }
+`;
+
+export const userResolvers = {
+  Query: {
+    userList: async () => {
+      try {
+        return await prisma.user.findMany();
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+
+  Mutation: {
+    createUser: async (_parent, { input }) => {
+      try {
+        return await prisma.user.create({ data: input });
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+};
